1ebe97205d21f87a66e249add264dfa526e372a4
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <wilsongonzalez.ag@academia.umb.edu.co>
2019-06-09 19:48:56 -0500

    Implements firebase method

---
+++ src/app/services/notes.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase} from "@angular/fire/database";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  constructor(public angularFireDatabase: AngularFireDatabase) {
+  }
+
+  note = [];
+
+  public getNotes() {
+    return this.angularFireDatabase.list('/notes/');
+  }
+
+  public getNote(id) {
+    return this.angularFireDatabase.object('/notes/' + id);
+  }
+
+  public createNote(note) {
+    return this.angularFireDatabase.database.ref('/notes/' + note.id).set(note);
+  }
+
+  public editNote(note) {
+    return this.angularFireDatabase.database.ref('/notes/' + note.id).set(note);
+  }
+
+  public deleteNote(note) {
+    return this.angularFireDatabase.database.ref('/notes/' + note.id).remove();
+  }
+}
